9be5ee4856b3c91d3c6c5e75b09e0fc3eb634aca
Abraham Rudy <aberudy@gmail.com>
2017-05-06 21:28:25 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
 
-function nestedTarget(selector, target) {
-return `document.getElementById(#${selector}).querySelector(.target)`
+function nestedTarget() {
+return document.querySelector('main #nested .target')
+}
+
+function increaseRankBy(n) {
+  const bank = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0, l = bank.length; i < l; i++) {
+  bank[i].innerHTML = parseInt(bank[i].innerHTML) + n
+}
+return
+}
+
+function deepestChild() {
+  var area = document.getElementById('grand-node');
+  return area.querySelector('div div div div')
 }
